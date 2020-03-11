cca3ebad286c6d16d6e8b76f254829c295891bc7
dglazier <Derek.Glazier@glasgow.ac.uk>
2020-03-11 10:21:06 +0000

    remove CHANZER

--- core/src/chanser.cpp
+++ core/src/chanser.cpp
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
 
   TString CHANZER=gSystem->Getenv("CHANSER");
 
-   app->ProcessLine(".x $CHANZER/macros/Load.C");
+   app->ProcessLine(".x $CHANSER/macros/Load.C");
    std::cout<<"Loading Final State class : "<<finalState<<std::endl;
    if(finalState.Length()&&finalState.Contains(':')){
      app->ProcessLine(Form("Loader::CompileClass(\"%s\")",finalState.Data()));
