af229ce8c3583480f96e35534907cb9859e37034
dglazier <Derek.Glazier@glasgow.ac.uk>
2020-05-17 09:21:24 +0100

    correct   FTel_pol4_ECorrection for missing /p_mag factor

--- actions/FTel_pol4_ECorrection.h
+++ actions/FTel_pol4_ECorrection.h
@@ -11,16 +11,22 @@ namespace chanser{
   FTel_pol4_ECorrection(Double_t a0,Double_t a1,Double_t a2,Double_t a3,Double_t a4):
     _a0{a0},_a1{a1},_a2{a2},_a3{a3},_a4{a4}{};
       
-      
+
+    	auto p_electron_pre_correction = el;
+	double p_mag = p_electron_pre_correction.P();
+	double p_mag_corrected = (p_mag-0.03689+0.1412*p_mag-0.04316*pow(p_mag,2)+0.007046*pow(p_mag,3)-0.0004055*pow(p_mag,4))/p_mag;
+	el.SetXYZM(el.X()*p_mag_corrected,el.Y()*p_mag_corrected,el.Z()*p_mag_corrected,0.00051099891);
+
     void ParticleCorrect(BaseParticle* part) const noexcept override{
       if(static_cast<CLAS12Particle*>(part)->CLAS12()->getRegion()!=clas12::FT)
 	return; //only FT e-
       
       Double_t p_mag = part->P4p()->P();
-       
-      Double_t p_mag_corrected = (_a0 + _a1*p_mag + _a2*p_mag*p_mag
-				+ _a3*p_mag*p_mag*p_mag
-				+ _a4*p_mag*p_mag*p_mag*p_mag);
+      if(pmag==0)return;
+      
+      Double_t p_mag_corrected = (_a0/p_mag + _a1 + _a2*p_mag
+				+ _a3*p_mag*p_mag
+				+ _a4*p_mag*p_mag*p_mag);
     
       part->SetP(p_mag_corrected);
 
