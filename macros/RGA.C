13e3cf8305bae0f132767e7b516418d94efa6f89
Robert Wishart <robertw@npc77.physics.gla.ac.uk>
2020-10-07 09:08:28 +0100

    Updated RGA.C

--- macros/RGA.C
+++ macros/RGA.C
@@ -5,103 +5,161 @@ void RGA(CLAS12FinalState* FS){
    */
   ParticleCutsManager epfcl{"ePCALFiducialLoose",0};
   epfcl.AddParticleCut("e-", new FiducialCutPCAL_uvw(9));
-  FS->RegisterPostKinAction(epfcl);
+  FS->RegisterPostTopoAction(epfcl);
 
   /*
    * Medium (14cm) cut on PCAL Fiducial region, applied to electrons. 
    */
   ParticleCutsManager epfcm{"ePCALFiducialMedium",0};
   epfcm.AddParticleCut("e-", new FiducialCutPCAL_uvw(14));
-  FS->RegisterPostKinAction(epfcm);
+  FS->RegisterPostTopoAction(epfcm);
 
   /*
    * Tight (19cm) cut on PCAL Fiducial region, applied to electrons. 
    */
   ParticleCutsManager epfct{"ePCALFiducialTight",0};
   epfct.AddParticleCut("e-", new FiducialCutPCAL_uvw(19));
-  FS->RegisterPostKinAction(epfct);
+  FS->RegisterPostTopoAction(epfct);
 
   /*
    * Loose (9cm) cut on PCAL Fiducial region, applied to photons. 
    */
   ParticleCutsManager gpfcl{"gammaPCALFiducialLoose",0};
   gpfcl.AddParticleCut("gamma", new FiducialCutPCAL_uvw(9));
-  FS->RegisterPostKinAction(gpfcl);
+  FS->RegisterPostTopoAction(gpfcl);
 
   /*
    * Medium (14cm) cut on PCAL Fiducial region, applied to photons. 
    */
   ParticleCutsManager gpfcm{"gammaPCALFiducialMedium",0};
   gpfcm.AddParticleCut("gamma", new FiducialCutPCAL_uvw(14));
-  FS->RegisterPostKinAction(gpfcm);
+  FS->RegisterPostTopoAction(gpfcm);
 
   /*
    * Tight (19cm) cut on PCAL Fiducial region, applied to photons. 
    */
   ParticleCutsManager gpfct{"gammaPCALFiducialTight",0};
   gpfct.AddParticleCut("gamma", new FiducialCutPCAL_uvw(19));
-  FS->RegisterPostKinAction(gpfct);
+  FS->RegisterPostTopoAction(gpfct);
 
   /*
    * Recommended cut on the electron z-vertex position.
+   * -18, 10 for outbending 
+   * -13, 12 for inbending
    */
   ParticleCutsManager zVertex{"ZVertex",0};
   zVertex.AddParticleCut("e-", new ZVertexCut(-13,12));
-  FS->RegisterPostKinAction(zVertex);
+  FS->RegisterPostTopoAction(zVertex);
 
   /*
    * DC Fiducial cuts for inbending hadrons
-   * across three layers of the DC
+   * across three layers of the DC (layer1)
    */
-  ParticleCutsManager DCFidHadInbend{"DC_Fiducial_Hadron_Inbend", 0}
-  DCFidHadInbend.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(6, -1));
-  DCFidHadInbend.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(6, -1));
-  DCFidHadInbend.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(18, -1));
-  DCFidHadInbend.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(18, -1));
-  DCFidHadInbend.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(36, -1));
+  ParticleCutsManager DCFidHadInbendL1{"DC_Fiducial_Hadron_Inbend_L1", 0};
+  DCFidHadInbendL1.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(6, -1));
+  DCFidHadInbendL1.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(6, -1));
+  FS->RegisterPostTopoAction(DCFidHadInbendL1);
 
-  DCFidHadInbend.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(36, -1));
-  FS->RegisterPostKinAction(DCFidHadInbend);
+  /*
+   * DC Fiducial cuts for inbending hadrons
+   * across three layers of the DC (layer2)
+   */
+  ParticleCutsManager DCFidHadInbendL2{"DC_Fiducial_Hadron_Inbend_L2", 0};
+  DCFidHadInbendL2.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(18, -1));
+  DCFidHadInbendL2.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(18, -1));
+  FS->RegisterPostTopoAction(DCFidHadInbendL2);
+
+  /*
+   * DC Fiducial cuts for inbending hadrons
+   * across three layers of the DC (layer3)
+   */
+  ParticleCutsManager DCFidHadInbendL3{"DC_Fiducial_Hadron_Inbend_L3", 0};
+  DCFidHadInbendL3.AddParticleCut("pi+", new DC_FiducialCut_ThetaPhi(36, -1));
+  DCFidHadInbendL3.AddParticleCut("pi-", new DC_FiducialCut_ThetaPhi(36, -1));
+  FS->RegisterPostTopoAction(DCFidHadInbendL3);
+  
+  /*
+   * DC Fiducial cuts for inbending electrons
+   * across three layers of the DC (layer1)
+   */
+  ParticleCutsManager DCFidElecInbendL1{"DC_Fiducial_Electron_InbendL1", 0};
+  DCFidElecInbendL1.AddParticleCut("e-", new DC_FiducialCut_XY(6, -1));
+  FS->RegisterPostTopoAction(DCFidElecInbendL1);
 
   /*
    * DC Fiducial cuts for inbending electrons
-   * across three layers of the DC
+   * across three layers of the DC (layer2)
    */
-  ParticleCutsManager DCFidElecInbend{"DC_Fiducial_Electron_Inbend", 0}
-  DCFidElecInbend.AddParticleCut("e-", new DC_FiducialCut_XY(6, -1));
-  DCFidElecInbend.AddParticleCut("e-", new DC_FiducialCut_XY(18, -1));
-  DCFidElecInbend.AddParticleCut("e-", new DC_FiducialCut_XY(36, -1));
-  FS->RegisterPostKinAction(DCFidElecInbend);
+  ParticleCutsManager DCFidElecInbendL2{"DC_Fiducial_Electron_InbendL2", 0};
+  DCFidElecInbendL2.AddParticleCut("e-", new DC_FiducialCut_XY(18, -1));
+  FS->RegisterPostTopoAction(DCFidElecInbendL2);
+
+  /*
+   * DC Fiducial cuts for inbending electrons
+   * across three layers of the DC (layer3)
+   */
+  ParticleCutsManager DCFidElecInbendL3{"DC_Fiducial_Electron_InbendL3", 0};
+  DCFidElecInbendL3.AddParticleCut("e-", new DC_FiducialCut_XY(36, -1));
+  FS->RegisterPostTopoAction(DCFidElecInbendL3);
+  
 
   /*
    * DC Fiducial cuts for outbending hadrons
-   * across three layers of the DC
+   * across three layers of the DC (layer1)
    */
-  ParticleCutsManager DCFidHadOutbend{"DC_Fiducial_Hadron_Outbend", 0}
-  DCFidHadOutbend.AddParticleCut("pi+", new DC_FiducialCut_XY(6, 1));
-  DCFidHadOutbend.AddParticleCut("pi-", new DC_FiducialCut_XY(6, 1));
-  DCFidHadOutbend.AddParticleCut("pi+", new DC_FiducialCut_XY(18, 1));
-  DCFidHadOutbend.AddParticleCut("pi-", new DC_FiducialCut_XY(18, 1));
-  DC1FidHadOutbend.AddParticleCut("pi+", new DC_FiducialCut_XY(36, 1));
-  DCFidHadOutbend.AddParticleCut("pi-", new DC_FiducialCut_XY(36, 1));
-  FS->RegisterPostKinAction(DCFidHadOutbend);
+  ParticleCutsManager DCFidHadOutbendL1{"DC_Fiducial_Hadron_Outbend_L1", 0};
+  DCFidHadOutbendL1.AddParticleCut("pi+", new DC_FiducialCut_XY(6, 1));
+  DCFidHadOutbendL1.AddParticleCut("pi-", new DC_FiducialCut_XY(6, 1));
+  FS->RegisterPostTopoAction(DCFidHadOutbendL1);
+
+  /*
+   * DC Fiducial cuts for outbending hadrons
+   * across three layers of the DC (layer2)
+   */
+  ParticleCutsManager DCFidHadOutbendL2{"DC_Fiducial_Hadron_Outbend_L2", 0};
+  DCFidHadOutbendL2.AddParticleCut("pi+", new DC_FiducialCut_XY(18, 1));
+  DCFidHadOutbendL2.AddParticleCut("pi-", new DC_FiducialCut_XY(18, 1));
+  FS->RegisterPostTopoAction(DCFidHadOutbendL2);
+
+  /*
+   * DC Fiducial cuts for outbending hadrons
+   * across three layers of the DC (layer3)
+   */
+  ParticleCutsManager DCFidHadOutbendL3{"DC_Fiducial_Hadron_Outbend_L3", 0};
+  DCFidHadOutbendL3.AddParticleCut("pi+", new DC_FiducialCut_XY(36, 1));
+  DCFidHadOutbendL3.AddParticleCut("pi-", new DC_FiducialCut_XY(36, 1));
+  FS->RegisterPostTopoAction(DCFidHadOutbendL3);
 
   /*
    * DC Fiducial cuts for outbending electrons
-   * across three layers of the DC
+   * across three layers of the DC (layer1)
    */
-  ParticleCutsManager DCFidElecOutbend{"DC_Fiducial_Electron_Outbend", 0}
-  DCFidElecOutbend.AddParticleCut("e-", new DC_FiducialCut_XY(6, 1));
-  DCFidElecOutbend.AddParticleCut("e-", new DC_FiducialCut_XY(18, 1));
-  DCFidElecOutbend.AddParticleCut("e-", new DC_FiducialCut_XY(36, 1));
-  FS->RegisterPostKinAction(DCFidElecOutbend);
+  ParticleCutsManager DCFidElecOutbendL1{"DC_Fiducial_Electron_Outbend_L1", 0};
+  DCFidElecOutbendL1.AddParticleCut("e-", new DC_FiducialCut_XY(6, 1));
+  FS->RegisterPostTopoAction(DCFidElecOutbendL1);
 
   /*
-   * Differnce between hadron and electron vertex difference cut
+   * DC Fiducial cuts for outbending electrons
+   * across three layers of the DC (layer2)
+   */
+  ParticleCutsManager DCFidElecOutbendL2{"DC_Fiducial_Electron_Outbend_L2", 0};
+  DCFidElecOutbendL2.AddParticleCut("e-", new DC_FiducialCut_XY(18, 1));
+  FS->RegisterPostTopoAction(DCFidElecOutbendL2);
+
+  /*
+   * DC Fiducial cuts for outbending electrons
+   * across three layers of the DC (layer3)
+   */
+  ParticleCutsManager DCFidElecOutbendL3{"DC_Fiducial_Electron_Outbend_L3", 0};
+  DCFidElecOutbendL3.AddParticleCut("e-", new DC_FiducialCut_XY(36, 1));
+  FS->RegisterPostTopoAction(DCFidElecOutbendL3);
+
+  /*
+   * Difference between hadron and electron vertex difference cut
    */
   ParticleCutsManager pcmVertexDiff{"ElHadVertexDiff", 0};
   pcmVertexDiff.AddParticleCut("pi-", new TwoParticleVertexCut("Electron", 20));
-  pcmVertexDiff.AddParticleCut("pi+", new TwoParticleVertexCut("Electron", 20))
+  pcmVertexDiff.AddParticleCut("pi+", new TwoParticleVertexCut("Electron", 20));
   FS->RegisterPostTopoAction(pcmVertexDiff);
 
   /*
@@ -113,12 +171,12 @@ void RGA(CLAS12FinalState* FS){
   FS->RegisterPostTopoAction(pcmChi2Pid);
 
   /*
-   * Hadron chi2Pid cuts (standard)
+   * Hadron chi2Pid cuts (strict)
    */
-  ParticleCutsManager pcmChi2Pid{"HadronChi2Pid", 0};
-  pcmChi2Pid.AddParticleCut("pi-", new HadronChi2PID(2));
-  pcmChi2Pid.AddParticleCut("pi+", new HadronChi2PID(2));
-  FS->RegisterPostTopoAction(pcmChi2Pid);
+  ParticleCutsManager pcmChi2PidStrict{"HadronChi2PidStrict", 0};
+  pcmChi2PidStrict.AddParticleCut("pi-", new HadronChi2PID(2));
+  pcmChi2PidStrict.AddParticleCut("pi+", new HadronChi2PID(2));
+  FS->RegisterPostTopoAction(pcmChi2PidStrict);
 
   /*
    * Photon identification refinement 
@@ -127,6 +185,6 @@ void RGA(CLAS12FinalState* FS){
   pcmPhotonRef.AddParticleCut("gamma", new PhotonIDRefinementCut(0.1));
   FS->RegisterPostTopoAction(pcmPhotonRef);
 
-  FS->WriteToFile("eep_config.root");
+  FS->WriteToFile("ALLALL.root");
   FS->Print();
 }
