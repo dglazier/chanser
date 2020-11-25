5e347aa52d8a68f73c74e89be03a30b54ebba892
Adam Thornton <ademus4@gmail.com>
2020-11-25 20:54:39 +0000

    Moved fit values, cut only valid for FD

--- rga_actions/Cut_RefineElectronID.h
+++ rga_actions/Cut_RefineElectronID.h
@@ -24,6 +24,19 @@ namespace chanser{
     Bool_t ParticleCut(const chanser::BaseParticle* part) const noexcept override{
       auto c12p = static_cast<const chanser::CLAS12Particle*>(part);
       auto c12=c12p->CLAS12();
+      if(c12->getRegion()!=clas12::FD) return true; //cut only applies to FD
+
+      double ecal_e_sampl_mu[3][6] = {
+        {  0.2531 ,  0.2550 ,  0.2514 ,  0.2494,  0.2528 ,  0.2521 },
+        { -0.6502 , -0.7472 , -0.7674 , -0.4913, -0.3988 , -0.703  },
+        {  4.939  ,  5.350  ,  5.102  ,  6.440 ,  6.149  ,  4.957  }
+      };
+
+      double ecal_e_sampl_sigma[3][6] = {
+          {  2.726e-3 ,  4.157e-3 ,  5.222e-3 , 5.398e-3 ,  8.453e-3 ,  6.533e-3 },
+          {  1.062    ,  0.859    ,  0.5564   , 0.6576   ,  0.3242   ,  0.4423   },
+          { -4.089    , -3.318    , -2.078    ,-2.565    , -0.8223   , -1.274    }
+      };
 
       //variables for calc
       double sigma_range = 3.5;
@@ -36,29 +49,28 @@ namespace chanser{
       double part_p = part->P4().P();
       double ECIN_en = c12->cal(clas12::ECIN)->getEnergy();
       double ECOUT_en = c12->cal(clas12::ECOUT)->getEnergy();
-      double EC_en_total = ECIN_en + ECOUT_en;
+      double EC_en_total = ECIN_en + ECOUT_en;      
       double PCAL_en = c12->cal(clas12::PCAL)->getEnergy();
-      int PCAL_sec = c12->cal(clas12::PCAL)->getSector()-1;
+      int PCAL_sec = c12->cal(clas12::PCAL)->getSector();
 
       //calc the mean and sigma, for specific sector -> method from S. Dhiel
-      mean = _ecal_e_sampl_mu[0][PCAL_sec] + 
-        (_ecal_e_sampl_mu[1][PCAL_sec] / 1000*TMath::Power(part_p-_ecal_e_sampl_mu[2][PCAL_sec],2));
-      sigma = _ecal_e_sampl_sigma[0][PCAL_sec] + 
-        (_ecal_e_sampl_sigma[1][PCAL_sec] / (10*(part_p-_ecal_e_sampl_sigma[2][PCAL_sec])));
+      mean = ecal_e_sampl_mu[0][PCAL_sec] + 
+        (ecal_e_sampl_mu[1][PCAL_sec] / 1000*TMath::Power(part_p-ecal_e_sampl_mu[2][PCAL_sec],2));
+      sigma = ecal_e_sampl_sigma[0][PCAL_sec] + 
+        (ecal_e_sampl_sigma[1][PCAL_sec] / (10*(part_p-ecal_e_sampl_sigma[2][PCAL_sec])));
       upper_lim_total = mean + sigma_range * sigma;
       lower_lim_total = mean - sigma_range * sigma;
 
       //electron within 3.5 sigma band
-      bool pass_band = (EC_en_total/part_p <= upper_lim_total) && 
-        (EC_en_total/part_p >= lower_lim_total);
-
+      bool pass_band = ((EC_en_total/part_p) <= upper_lim_total) && ((EC_en_total/part_p) >= lower_lim_total);
+      
       //triangular cut as seen in RGA analysis note fig 78
       if(part_p < 4.5){ 
         pass_triangle = true;
       } else {
         pass_triangle = ECIN_en/part_p > (0.2 - PCAL_en/part_p);
       }
-      return (pass_band && pass_triangle)? kTRUE : kFALSE;
+      return pass_band && pass_triangle;
     }
 
     void Print(Option_t* option = "")const final{
@@ -67,9 +79,6 @@ namespace chanser{
 
   private:
     
-    double _ecal_e_sampl_mu[3][6];
-    double _ecal_e_sampl_sigma[6][6];
-
     ClassDefOverride(chanser::Cut_RefineElectronID,1);
 
   };//class Cut_RefineElectronID
